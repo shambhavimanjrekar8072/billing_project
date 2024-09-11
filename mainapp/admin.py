979141d7621e7ab80d782d00768324f1f4cad032
mehul1908 <135810448+mehul1908@users.noreply.github.com>
2024-09-11 22:19:10 +0530

    11.09.2024.1

---
+++ mainapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+admin.site.register(models.Billing)
+admin.site.register(models.Course)
+admin.site.register(models.Subject)
+admin.site.register(models.User)
---
+++ mainapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
+# Create your models here.
+
+class User(AbstractUser):
+	address = models.CharField(max_length=50)
+	college_name = models.CharField(max_length=50 ,  default="International Institute of Professional Studies")
+	pan_card = models.CharField(max_length=10)
+	bank = models.CharField(max_length=20)
+	bank_addr = models.CharField(max_length=50)
+	bank_ac_no = models.IntegerField(default=1234)
+	ifsc_code = models.CharField(max_length=11)
+
+	def __str__(self) -> str:
+		return self.username
+
+class Course(models.Model):
+	id = models.CharField(max_length=4 , primary_key=True)
+	name =  models.CharField(max_length=10)
+
+	def __str__(self) -> str:
+		return self.id + self.name
+	
+class Subject(models.Model):
+	id =  models.CharField(max_length=6 , primary_key=True)
+	name = models.CharField(max_length=20)
+	sem = models.IntegerField()
+	course = models.ForeignKey(to=Course , on_delete=models.CASCADE)
+
+class Billing(models.Model):
+	teacher = models.ForeignKey(to=User  , on_delete=models.DO_NOTHING)
+	course =  models.ForeignKey(to=Course , on_delete=models.DO_NOTHING)
+	subject = models.ForeignKey(to=Subject , on_delete=models.DO_NOTHING)
+	date = models.DateTimeField(default=datetime.now)
